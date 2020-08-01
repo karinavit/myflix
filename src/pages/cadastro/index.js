24b51bc07242d678c2930d610282f30cb2a3e337
karinavit <ka.vitangelo@gmail.com>
2020-08-01 12:05:37 -0300

    Inserido o Carousel, instalado o styled-components

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -6,6 +6,7 @@ function CadastroVideo(){
     return (
         <PageDefault>
           <h1>Cadastro de Video </h1> 
+          
 
           <Link to="/cadastro/categoria">
             Cadastrar Categoria
